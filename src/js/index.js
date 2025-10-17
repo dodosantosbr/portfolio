d50ca65c351c3d0e6317cdbcc207e8e72528e673
Douglas Sales <sallesard@gmail.com>
2025-10-17 20:36:43 -0300

    feat:alterado logos e cores

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
 
@@ -16,23 +15,4 @@ function esconderbtn() {
 
 window.onload = function() {
     document.querySelector('.projetos .titulo').classList.add('aparecer');
-=======
-const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
-const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
-
-esconderbtn();
-
-function esconderbtn() {
-    botaoMostrarProjetos.addEventListener('click', () => {
-        projetosInativos.forEach(projetoInativo => {
-            projetoInativo.classList.add('ativo');
-        });
-
-        botaoMostrarProjetos.classList.add('remover');
-    });
-}
-
-window.onload = function() {
-    document.querySelector('.projetos .titulo').classList.add('aparecer');
->>>>>>> 5c13d45 (Feat: foi adicionado as novas funcionalidades, alterado a cor do background e alterado as skills)
 };
