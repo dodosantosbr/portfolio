ba80ad6030062d2cdd82e4235cb04e4bd40f8528
Douglas Sales <sallesard@gmail.com>
2025-10-17 20:30:34 -0300

    Feat: foi adicionado as novas funcionalidades, alterado a cor do background e alterado as skills

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
 
@@ -15,4 +16,23 @@ function esconderbtn() {
 
 window.onload = function() {
     document.querySelector('.projetos .titulo').classList.add('aparecer');
+=======
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
+
+esconderbtn();
+
+function esconderbtn() {
+    botaoMostrarProjetos.addEventListener('click', () => {
+        projetosInativos.forEach(projetoInativo => {
+            projetoInativo.classList.add('ativo');
+        });
+
+        botaoMostrarProjetos.classList.add('remover');
+    });
+}
+
+window.onload = function() {
+    document.querySelector('.projetos .titulo').classList.add('aparecer');
+>>>>>>> 5c13d45 (Feat: foi adicionado as novas funcionalidades, alterado a cor do background e alterado as skills)
 };
